3c23eb1f1df26dd5af1690c414a89e6d84614317
Mor <nodemaand17@gmail.com>
2019-10-22 00:54:28 +0300

    Updates

---
+++ precache-manifest.7eb051ad302ee0ed92b33b656c4e740d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "160848e16b25159ed09d86889f95850e",
+    "url": "/adi/index.html"
+  },
+  {
+    "revision": "10e0ab68c0316ad3772f",
+    "url": "/adi/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "34b5c141aa5a40d09170",
+    "url": "/adi/static/css/main.30c289bb.chunk.css"
+  },
+  {
+    "revision": "10e0ab68c0316ad3772f",
+    "url": "/adi/static/js/2.316f611d.chunk.js"
+  },
+  {
+    "revision": "34b5c141aa5a40d09170",
+    "url": "/adi/static/js/main.900c89ef.chunk.js"
+  },
+  {
+    "revision": "d1bf2343db0a95fa2d2e",
+    "url": "/adi/static/js/runtime-main.8382ee58.js"
+  }
+]);
